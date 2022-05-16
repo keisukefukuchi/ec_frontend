af62a03f44947afbcfbfa4e6c5f806f28e45b65d
keisukefukuchi <kekke53530708@gmail.com>
2022-05-17 08:58:46 +0900

    追加：認証ページのデザイン

--- components/Login.vue
+++ components/Login.vue
@@ -5,34 +5,50 @@
       <h3 class="login-head">会員のお客様</h3>
     </div>
     <div class="login-form">
-      <p class="login-content">メールアドレスとパスワードを入力してログインしてください。</p>
+      <p class="login-content">
+        メールアドレスとパスワードを入力してログインしてください。
+      </p>
       <validation-observer ref="obs" v-slot="ObserverProps">
-      <validation-provider v-slot="{ errors }" rules="required|email">
-        <label class="email" for="email">メールアドレス</label>
-        <input v-model="email" id="email" type="email" name="メールアドレス" />
-        <div class="error">{{ errors[0] }}</div>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        vid="passwordConfirm"
-        rules="required|min:8|alpha_dash"
-      >
-        <label class="password" for="password">パスワード</label>
-        <input
-          v-model="password"
-          id="password"
-          type="password"
-          name="パスワード"
-        />
-        <div class="error">{{ errors[0] }}</div>
-      </validation-provider>
-      <button
-        type="button"
-        :disabled="ObserverProps.invalid || !ObserverProps.validated"
-      >
-        送信
-      </button>
-    </validation-observer>
+        <validation-provider
+          v-slot="{ errors }"
+          rules="required|email"
+        >
+          <div class="login-email">
+            <label class="email" for="email">メールアドレス</label>
+            <input
+              v-model="email"
+              id="email"
+              type="email"
+              name="メールアドレス"
+            />
+          </div>
+          <div class="error">{{ errors[0] }}</div>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          vid="passwordConfirm"
+          rules="required|min:8|alpha_dash"
+        >
+          <div class="login-password">
+            <label class="password" for="password">パスワード</label>
+            <input
+              v-model="password"
+              id="password"
+              type="password"
+              name="パスワード"
+            />
+          </div>
+          <div class="error">{{ errors[0] }}</div>
+        </validation-provider>
+        <div class="login-button">
+          <button
+            type="button"
+            :disabled="ObserverProps.invalid || !ObserverProps.validated"
+          >
+            ログイン
+          </button>
+        </div>
+      </validation-observer>
     </div>
   </div>
 </template>
--- components/Register.vue
+++ components/Register.vue
@@ -1,50 +1,59 @@
 <template>
   <div class="register-container">
-    <validation-observer ref="obs" v-slot="ObserverProps">
-      <validation-provider v-slot="{ errors }" rules="required|min:4|alpha">
-        <label for="username">お名前</label>
-        <input v-model="username" id="username" type="text" name="名前" />
-        <div class="error">{{ errors[0] }}</div>
-      </validation-provider>
-      <validation-provider v-slot="{ errors }" rules="required|email">
-        <label for="email">メールアドレス</label>
-        <input v-model="email" id="email" type="email" name="メールアドレス" />
-        <div class="error">{{ errors[0] }}</div>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        vid="passwordConfirm"
-        rules="required|min:8|alpha_dash"
-      >
-        <label for="password">パスワード</label>
-        <input
-          v-model="password"
-          id="password"
-          type="password"
-          name="パスワード"
-        />
-        <div class="error">{{ errors[0] }}</div>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        rules="confirmed:passwordConfirm"
-      >
-        <label for="passwordConfirm">パスワード（確認）</label>
-        <input
-          v-model="passwordConfirm"
-          id="passwordConfirm"
-          type="password"
-          name="確認パスワード"
-        />
-        <div class="error">{{ errors[0] }}</div>
-      </validation-provider>
-      <button
-        type="button"
-        :disabled="ObserverProps.invalid || !ObserverProps.validated"
-      >
-        送信
-      </button>
-    </validation-observer>
+    <div class="register-head-group">
+      <h1 class="register-header">新規会員登録</h1>
+      <h3 class="register-head">初めてご利用の方・会員以外の方</h3>
+    </div>
+    <div class="register-form">
+      <p class="register-content">
+        初めてご利用のお客様は、こちらから会員登録を行って下さい。
+      </p>
+      <validation-observer ref="obs" v-slot="ObserverProps">
+        <validation-provider v-slot="{ errors }" rules="required|email">
+          <div class="register-email">
+            <label class="email" for="email">メールアドレス</label>
+            <input
+              v-model="email"
+              id="email"
+              type="email"
+              name="メールアドレス"
+            />
+          </div>
+          <div class="error">{{ errors[0] }}</div>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          vid="passwordConfirm"
+          rules="required|min:8|alpha_dash"
+        >
+          <div class="register-password">
+            <label class="password" for="password">パスワード</label>
+            <input
+              v-model="password"
+              id="password"
+              type="password"
+              name="パスワード"
+            />
+          </div>
+          <div class="error">{{ errors[0] }}</div>
+        </validation-provider>
+        <validation-provider v-slot="ProviderProps" rules="required">
+          <div class="register-name">
+            <label for="name">お名前</label>
+            <input v-model="name" id="name" type="text" name="名前" />
+          </div>
+          <div class="error">{{ ProviderProps.errors[0] }}</div>
+        </validation-provider>
+        <div class="register-button">
+          <button
+            type="button"
+            :disabled="ObserverProps.invalid || !ObserverProps.validated"
+          >
+            新規会員登録
+          </button>
+        </div>
+      </validation-observer>
+    </div>
   </div>
 </template>
 
